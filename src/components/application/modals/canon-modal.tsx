06e92593b6e2ed6e091bdabfaacb248488bf706b
rafaelortet <rafael.ortet@84000.co>
2026-06-18 21:44:48 +0200

    Update the Navigational Modal component

--- src/components/application/modals/canon-modal.tsx
+++ src/components/application/modals/canon-modal.tsx
@@ -12,9 +12,9 @@ export const ModalOverlay = (props: AriaModalOverlayProps) => {
             {...props}
             className={(state) =>
                 cx(
-                    "fixed inset-0 z-100 flex w-full h-full bg-overlay/10 px-13 pt-17 outline-hidden",
-                    state.isEntering && "duration-300 ease-out animate-in fade-in",
-                    state.isExiting && "duration-200 ease-in animate-out fade-out",
+                    "fixed inset-0 z-100 flex w-full h-full bg-overlay/20 px-13 pt-20 outline-hidden",
+                    state.isEntering && "duration-400 ease-out animate-in fade-in",
+                    state.isExiting && "duration-400 ease-in animate-out fade-out",
                     typeof props.className === "function" ? props.className(state) : props.className,
                 )
             }
@@ -27,9 +27,9 @@ export const Modal = (props: AriaModalOverlayProps) => (
         {...props}
         className={(state) =>
             cx(
-                "flex max-h-full w-full outline-hidden border border-secondary",
-                state.isEntering && "duration-300 ease-out animate-in slide-in-from-bottom",
-                state.isExiting && "duration-200 ease-in animate-out slide-out-to-bottom",
+                "flex max-h-full w-full outline-hidden border border-secondary rounded-t-lg",
+                state.isEntering && "duration-400 ease-out animate-in slide-in-from-bottom",
+                state.isExiting && "duration-400 ease-in animate-out slide-out-to-bottom",
                 typeof props.className === "function" ? props.className(state) : props.className,
             )
         }
