4877de8d019a73c528c9ff439f6e572804e48b2a
rafaelortet <rafael.ortet@84000.co>
2026-07-17 16:48:24 +0700

    Update the Tibetan Canon landing page Hero section.

--- src/app/[locale]/(marketing)/scripture/tibetan-canons/page.tsx
+++ src/app/[locale]/(marketing)/scripture/tibetan-canons/page.tsx
@@ -8,9 +8,9 @@ export default function TibetanCanonsPage() {
     return (
         <>
             {/* Hero Section */}
-            <section className="relative flex flex-col items-center justify-center gap-4 mx-auto h-[calc(75vh)] bg-[url(/tibetan-buddhist-library-01.jpg)] bg-cover bg-bottom bg-no-repeat pt-16">
+            <section className="relative flex flex-col items-center justify-center gap-4 mx-auto h-[calc(76vh)] bg-[url(/tibetan-buddhist-library-01.jpg)] bg-cover bg-bottom bg-no-repeat pt-16">
                 {/* Overlay layer for easy customization of color/transparency */}
-                <div className="absolute inset-0 bg-linear-to-b from-brand-50 via-black/50 via-40% to-brand-50 dark:bg-linear-to-b dark:from-black/15 dark:via-black/50 dark:via-30% dark:to-black/10 z-0" aria-hidden="true" />
+                <div className="absolute inset-0 bg-linear-to-b from-brand-300 from-5% via-black/50 via-40% to-brand-600/40 to-100% dark:bg-linear-to-b dark:from-brand-900/80 dark:via-black/40 dark:via-35% dark:to-brand-950/50 z-0" aria-hidden="true" />
 
                 {/* Hero Inner Content */}
                 <div className="relative flex flex-col items-center justify-center gap-12 z-10">
@@ -23,18 +23,18 @@ export default function TibetanCanonsPage() {
                     <div className="mx-auto max-w-110 md:max-w-4xl px-6 md:pb-16 md:px-6 grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-10 lg:gap-14 items-start animate-[fadeUp_1s_ease_both] delay-450">
                         {/* Kangyur Card */}
                         <a href="#kangyur-divisions-gallery" className="cursor-pointer hover:scale-[1.06] transition-transform duration-400 h-full" aria-label="Go to Kangyur collection gallery">
-                            <div className="flex flex-col h-full items-center justify-start bg-radial-[at_50%_46%] from-brand-50/90 via-brand-200/95 via-40% to-brand-500/20 dark:bg-linear-to-b dark:bg-radial-[at_50%_46%] dark:from-brand-950/95 dark:via-brand-900/90 dark:via-30% dark:to-black/25 p-4 md:p-8 rounded-full drop-shadow-2xl">
+                            <div className="flex flex-col h-full items-center justify-start bg-radial-[at_50%_46%] from-brand-50/90 via-brand-200/95 via-40% to-brand-500/20 dark:bg-linear-to-b dark:bg-radial-[at_50%_46%] dark:from-brand-800 dark:via-brand-800/80 dark:via-30% dark:to-brand-700/70 to-80% p-4 md:p-8 rounded-full drop-shadow-2xl">
                                 <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} style={{ width: "auto", height: "auto" }} className="hidden md:block drop-shadow-xs drop-shadow-black/90 dark:drop-shadow-black/50 opacity-60" />
-                                <h2 className="text-display-md text-brand-800 dark:text-brand-200 text-center py-2">The Kangyur Collection</h2>
-                                <p className="text-md text-tertiary font-semibold text-center pb-4">The Kangyur is the collection of the translated words of the Buddha in Tibetan.</p>
+                                <h2 className="text-display-md font-light text-brand-800 dark:text-brand-50 text-center py-2">The Kangyur Collection</h2>
+                                <p className="text-md text-tertiary font-semibold text-center pb-4">The Kangyur is the Tibetan collection of translated Indian Scriptures containing the words of the Buddha.</p>
                             </div>
                         </a>
                         {/* Tengyur Card */}
                         <a href="#tengyur-divisions-gallery" className="cursor-pointer hover:scale-[1.06] transition-transform duration-400 h-full" aria-label="Go to Tengyur collection gallery">
-                            <div className="flex flex-col h-full items-center justify-start bg-radial-[at_50%_46%] from-brand-50/90 via-brand-200/95 via-40% to-brand-500/20 dark:bg-linear-to-b dark:bg-radial-[at_50%_46%] dark:from-brand-950/95 dark:via-brand-900/90 dark:via-30% dark:to-black/15 p-4 md:p-8 rounded-full drop-shadow-2xl">
+                            <div className="flex flex-col h-full items-center justify-start bg-radial-[at_50%_46%] from-brand-50/90 via-brand-200/95 via-40% to-brand-500/20 dark:bg-linear-to-b dark:bg-radial-[at_50%_46%] dark:from-brand-800 dark:via-brand-800/80 dark:via-30% dark:to-brand-700/70 to-80% p-4 md:p-8 rounded-full drop-shadow-2xl">
                                 <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} style={{ width: "auto", height: "auto" }} className="hidden md:block drop-shadow-xs drop-shadow-black/90 dark:drop-shadow-black/50 opacity-60" />
-                                <h2 className="text-display-md text-brand-800 dark:text-brand-200 text-center py-2">The Tengyur Collection</h2>
-                                <p className="text-md text-tertiary font-semibold text-center pb-4">The Tengyur is the collection of the translated Buddhist treatises in Tibetan.</p>
+                                <h2 className="text-display-md font-light text-brand-800 dark:text-brand-50 text-center py-2">The Tengyur Collection</h2>
+                                <p className="text-md text-tertiary font-semibold text-center pb-4">The Tengyur is the Tibetan collection of the translated Indian treatises and commnetaries on the words of the Buddha.</p>
                             </div>
                         </a>
                     </div>
