01763286cba57b5e993cb119938e39c7118d0930
rafaelortet <rafael.ortet@84000.co>
2026-07-31 15:19:46 +0700

    Adjustments to the Hero section of the Tibetan Canons. New class applied to the Mandala via globals.css.

--- src/app/[locale]/(marketing)/scripture/tibetan-canons/page.tsx
+++ src/app/[locale]/(marketing)/scripture/tibetan-canons/page.tsx
@@ -3,16 +3,15 @@ import { KangyurDivisionsSection } from '@/components/marketing/collection-secti
 import { TengyurDivisionsSection } from '@/components/marketing/collection-sections/tengyur-divisions-section';
 import { TibetanCanonsDiagram } from "@/components/marketing/sections/tibetan-canons-diagram";
 import { BlogSectionTibetanCanonOverviews } from '@/components-custom/blog/blog-tibetan-canon-overviews';
-import "@/styles/home-hero.css";
 
 export default function TibetanCanonsPage() {
     return (
         <>
             {/* Hero Section */}
-            <section className="relative flex flex-col gap-4 mx-auto h-[calc(100vh)] bg-[url(/tibetan-buddhist-library-01.jpg)] bg-cover bg-bottom bg-no-repeat pt-24 overflow-hidden">
+            <section className="relative flex flex-col gap-4 mx-auto h-[calc(100vh)] bg-[url(/tibetan-buddhist-library-01.jpg)] bg-cover bg-bottom bg-no-repeat pt-32 overflow-hidden">
                 {/* Overlay layer for easy customization of color/transparency */}
-                <div className="absolute inset-0 bg-linear-to-b from-black/80 from-15% via-black/15 via-40% to-black/50 to-100% z-0" aria-hidden="true" />
-<svg className="hero-mandala-right absolute z-0 top-20 right-0 w-110 h-110 opacity-60" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div className="absolute inset-0 bg-linear-to-b from-black/90 from-15% via-black/20 via-40% to-black/60 to-100% z-0" aria-hidden="true" />
+<svg className="hero-mandala-tibetan-canons absolute z-0 top-0 right-0 w-100 h-100 opacity-50" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="200" cy="200" r="196" stroke="#B08D57" strokeWidth="0.5" />
                     <circle cx="200" cy="200" r="170" stroke="#B08D57" strokeWidth="0.3" />
                     <circle cx="200" cy="200" r="140" stroke="#B08D57" strokeWidth="0.5" />
@@ -61,19 +60,19 @@ export default function TibetanCanonsPage() {
                     </g>
                     </svg>
                 {/* Hero Inner Content */}
-                <div className="mx-24 relative max-w-container flex flex-col gap-12 z-10">
+                <div className="mx-24 relative max-w-container flex flex-col gap-10 z-10">
                     
                     {/* Hero Title & Subtitle */}
                     <div className="md:flex flex-col gap-1 animate-[fadeUp_1s_ease_both] delay-300 ">
-                        <h1 className="text-display-xl font-extralight text-brand-400 md:text-display-3xl lg:text-display-4xl text-shadow-brand-950 text-shadow-sm">Tibetan Canon</h1>
-                        <p className="hero-subtitle text-xl font-light text-white w-110">Buddhist Indian scriptures and treatises translated into Tibetan.</p>
+                        <h1 className="text-display-xl font-extralight text-brand-400 md:text-display-3xl lg:text-display-4xl text-shadow-brand-950 text-shadow-sm">The Tibetan Canon</h1>
+                        <p className="hero-subtitle text-xl font-light text-white w-140">Buddhist Indian scriptures and treatises translated into Tibetan.</p>
                     </div>
                     {/* Tibetan Canons Cards */}
                     <div className="max-w-140 md:max-w-3xl md:pb-16 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-14 lg:gap-20 items-start animate-[fadeUp_1s_ease_both] delay-450">
                         {/* Kangyur Card */}
                         <a href="#kangyur-divisions-gallery" className="cursor-pointer hover:scale-[1.04] transition-transform duration-400 h-full" aria-label="Go to Kangyur collection gallery">
                             <div className="flex flex-col h-full">
-                                <Image src="/ornaments/golden-wheel-280px.webp" alt="wheel of dharma" width={80} height={80} style={{ width: "80px", height: "80px" }} className="hidden md:block opacity-100" />
+                                <Image src="/ornaments/golden-wheel-280px.webp" alt="wheel of dharma" width={70} height={70} style={{ width: "70px", height: "70px" }} className="hidden md:block opacity-100" />
                                 <h2 className="text-display-lg font-extralight text-brand-400 dark:text-brand-50 py-2">The Kangyur</h2>
                                 <p className="text-lg text-white font-light pb-4">Tibetan translations of Indidan Scriptures, incorporating the teachings of the Buddha.</p>
                             </div>
@@ -81,9 +80,9 @@ export default function TibetanCanonsPage() {
                         {/* Tengyur Card */}
                         <a href="#tengyur-divisions-gallery" className="cursor-pointer hover:scale-[1.04] transition-transform duration-400 h-full" aria-label="Go to Tengyur collection gallery">
                             <div className="flex flex-col h-full">
-                                <Image src="/ornaments/golden-wheel-280px.webp" alt="wheel of dharma" width={80} height={80} style={{ width: "80px", height: "80px" }} className="hidden md:block opacity-100" />
+                                <Image src="/ornaments/golden-wheel-280px.webp" alt="wheel of dharma" width={70} height={70} style={{ width: "70px", height: "70px" }} className="hidden md:block opacity-100" />
                                 <h2 className="text-display-lg font-extralight text-brand-400 dark:text-brand-50 py-2">The Tengyur</h2>
-                                <p className="text-md text-white font-light pb-4">Tibetan translations of Indian treatises that explain the Buddha's teachings.</p>
+                                <p className="text-lg text-white font-light pb-4">Tibetan translations of Indian treatises that explain the Buddha's teachings.</p>
                             </div>
                         </a>
                     </div>
